8fdbf99f330deafaabcb49750a7d5654d811bf9e
Pat Greninger <pgreninger@computer.org>
2014-07-13 17:11:46 -0400

    Add script files

---
+++ HowToRun.md
@@ -0,0 +1,35 @@
+## Required Files
+<ul>
+<li>**readPowerData.R** - this file reads the data file and is used by all the plot scripts
+<li>**plot1.R**
+<li>**plot2.R**
+<li>**plot3.R**
+<li>**plot4.R**
+</ul>
+
+Assuming the scripts and the data file are in your working directory you can enter the following command to create each plot:
+`source("plot#.R")`
+
+Each plot script creates a file with the corresponding plot number.
+
+
+### Plot 1
+
+
+![histogram of Global Active Power](plot1.png) 
+
+
+### Plot 2
+
+![plot of Global Active Power](plot2.png) 
+
+
+### Plot 3
+
+![plot sub metering locations](plot3.png) 
+
+
+### Plot 4
+
+![multiple figure plot](plot4.png) 
+
---
+++ readPowerData.R
@@ -0,0 +1,28 @@
+## This script reads household power consumption data for 2007-02-01 and 2007-02-02 from
+## https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+## downloaded on 2014-07-12. The file was pre-processed using grep to extract the dates
+## of interest. The full file contains 2,075,259 rows. The filtered data contains 2880 rows.
+## The following command was used to filter the data:
+##    grep ^[1-2]/2/2007 household_power_consumption.txt > household_power2.txt
+##
+## Note that the filtered data does not contain a header row.
+## The script assumes the household_power2.txt file is in your working directory
+## setwd("~/coursera/DataScience")
+
+# define column classes
+classes<-c(rep("character",2), rep("numeric",7))
+
+# read file - note that csv2 assumes "," as decimal separator so override default.
+power<-read.csv2("household_power2.txt", dec=".", header=FALSE, stringsAsFactors=FALSE, colClasses=classes)
+
+# get header row from original file and set column names 
+heading<-read.csv2("household_power_consumption.txt", header=FALSE, nrows=1)
+names(power2)<-heading
+
+# create datetime object from date and time strings
+datetime<-paste(power2$Date, power2$Time)
+datetime<-as.POSIXlt(datetime, format="%d/%m/%Y %H:%M:%S")
+
+
+
+
